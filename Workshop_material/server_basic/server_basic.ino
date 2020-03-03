8ce10a92e7822e22df9732916d9cf8929a64e7b9
yvedant <yadavvedant@gmail.com>
2020-03-03 17:48:56 +0530

    Changes in Server

--- Workshop_material/server_basic/server_basic.ino
+++ Workshop_material/server_basic/server_basic.ino
@@ -19,7 +19,7 @@ void setup() {
   Serial.println(WiFi.localIP());  //Print the local IP
  
   server.on("/other", []() {   //Define the handling function for the path
-    String a="<!DOCTYPE html><html><h1>Welcome to Avotrix</h1><p><h2>What we offer?</h2></p><ul><li>Seminars</li><li>Webinars</li><li>Workshops</li><li>IoT Courses</li><li>Splunk Courses</li><li>Hackathons</li><li>Internship opportunities</li><ul><p>Kindly visit our website <a href='http://www.avotrix.com'>Avotrix</a> to know more about us or can reach us directly on 7777004497</p></html>";
+    String a="<!DOCTYPE html> <html> <style> h1{ color: rgb(237,77,64); font-family: Arial, Helvetica, sans-serif; font-size: 350%; text-align: center; font-weight: bold; } h2{ color: rgb(45,62,80); font-family: Arial, Helvetica, sans-serif; text-align: center; font-weight: bold; } li{ 	font-family: Arial, Helvetica, sans-serif; 	color: rgb(45,62,80); 	font-weight: bold; } .b{ 	font-family: Arial, Helvetica, sans-serif; 	color: rgb(237,77,64); 	font-weight: bold; } </style> <body> 	<h1>Welcome to Avotrix</h1> 	<p><h2>What we offer?</h2></p> 	<ul> 		<li>Seminars</li> 		<li>Webinars</li> 		<li>Workshops</li> 		<li>IoT Courses</li> 		<li>Splunk Courses</li> 		<li>Hackathons</li> 		<li>Internship opportunities</li> 	</ul> 	<br><br><br> 	<footer><div class='b'>Kindly visit our website <a href='http://www.avotrix.com'>Avotrix</a> to know more about us or can reach us directly on 7777004497</div></footer> </body> </html>";
     
     server.send(200, "html", a);
  
